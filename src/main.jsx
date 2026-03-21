35443c689343a1048086d5cfa184c2789bffe03e
Kai2314 <adward2009kb@hotmail.com>
2026-03-21 23:07:24 +0800

    feat: Initialize React application, integrate Vercel Analytics, and add temporary chapter data files.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// ---Vercel 追蹤分析觀眾 ---
+import { inject } from '@vercel/analytics';
+inject();
+// -----------------------
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
